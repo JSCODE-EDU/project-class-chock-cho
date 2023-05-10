2b78111e3cc149c9cd2fcbd5e903dfb7a9e99461
chock-cho <christer10@ewhain.net>
2023-05-11 01:05:39 +0900

    1회차 미션: [환경설정]

---
+++ src/test/java/com/example/SpringBootCHO/SpringBootChoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootCHO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootChoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
